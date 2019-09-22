a1f71a6fe27e5b74ad7c5fd54c8956d2b0ed55ed
ablades <audaris@gmail.com>
2019-09-21 20:25:10 -0700

    finish page 2

---
+++ superheroes.py
@@ -0,0 +1,131 @@
+import random as r
+
+class Ability:
+
+    def __init__(self, name, attack_strength):
+        '''Create Instance Variables:
+          name:String
+          attack_strength: Integer
+       '''
+        self.name = name
+        self.attack_strength = attack_strength
+
+    def attack(self):
+        ''' Return a value between 0 and the value set by self.max_damage.'''
+        return r.randint(0, self.attack_strength)
+
+class Armor:
+    def __init__(self, name, max_block):
+        ''' Instantiate instance properties.
+            name: String
+            max_block: Integer
+        '''
+        self.name = name
+        self.max_block = max_block
+
+    def block(self):
+        ''' Return a random value between 0 and max_block
+        '''
+        return r.randint(0, self.max_block)
+
+class Hero:
+    def __init__(self, name, starting_health=100):
+        '''Instance properties:
+            abilities: List
+            armors: List
+            name: String
+            starting_health: Integer
+            current_health: Integer
+        '''
+        self.name = name
+        self.starting_health = starting_health
+        self.current_health = starting_health
+        self.abilities = []
+        self.armors = []
+    
+    def add_ability(self, ability):
+        ''' Add ability to abilities list '''
+        self.abilities.append(ability)
+
+    def attack(self):
+        ''' Calculate the total damage from all ability attacks
+            return: total: Integer
+        '''
+        total_damage = 0
+        for ability in self.abilities:
+            total_damage += ability.attack()
+
+        return total_damage
+
+    def add_armor(self, armor):
+        ''' Add armor to self.armors
+            Armor: Armor Object
+        '''
+        self.armors.append(Armor(armor.name, armor.max_block))
+    
+    def defend(self, damage_amt):
+        ''' Runs 'block' method on each armor.
+            Returns sum of all blocks
+        '''
+        total_blocked = 0
+        for armor in self.armors:
+            total_blocked += armor.block()
+
+        return total_blocked
+
+    def take_damage(self, damage):
+        ''' Updates self.current_health to reflect the damage minus defense.
+        '''
+        damage_taken = damage - self.defend(damage)
+        self.current_health -= damage_taken
+
+    def is_alive(self):
+        '''Return True or False depending on whether the hero is alive or not.
+        '''
+        if self.current_health > 0:
+            return True
+        else:
+            return False
+
+    def fight(self, opponent):
+        '''Current Hero will take turns fighting ther opponent passed in.'''
+        while self.is_alive() and opponent.is_alive():
+    
+            if len(self.abilities) == 0 and len(opponent.abilities) == 0:
+                print("Draw")
+                break
+
+            self.take_damage(opponent.attack())
+            opponent.take_damage(self.attack())
+
+
+        if self.current_health > opponent.current_health:
+            print(f"{self.name} Wins!!!")
+        else:
+            print(f"{opponent.name} Wins!!!")
+
+
+
+
+
+
+
+
+    
+
+ 
+        
+
+
+if __name__ == "__main__":
+    hero1 = Hero("Wonder Woman")
+    hero2 = Hero("Dumbledore")
+    ability1 = Ability("Super Speed", 300)
+    ability2 = Ability("Super Eyes", 130)
+    ability3 = Ability("Wizard Wand", 80)
+    ability4 = Ability("Wizard Beard", 20)
+    hero1.add_ability(ability1)
+    hero1.add_ability(ability2)
+    hero2.add_ability(ability3)
+    hero2.add_ability(ability4)
+    hero1.fight(hero2)
